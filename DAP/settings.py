b92c76fdabf71c100c60056d94a7fe3ee09f806a
차경화 <ishysy3333@gmail.com>
2020-04-22 21:37:58 +0900

    create project

--- DAP/settings.py
+++ DAP/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'userInfo',
 ]
 
 REST_FRAMEWORK = {
@@ -56,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'userInfo',
 ]
 
 ROOT_URLCONF = 'DAP.urls'
--- userInfo/models.py
+++ userInfo/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class userInfo(models.Model):
-    userId = models.CharField(max_length = 10)
-    password = models.charField(max_length = 10)
+class UserInfo(models.Model):
+    userId = models.CharField(max_length=10,primary_key=True)
+    password = models.charField(max_length=10)
+    email = models.EmailField(unique=True)
+    birth = models.DateField()
+    gender = models.IntegerField()
+
+class Meta:
+    ordering =['created']
